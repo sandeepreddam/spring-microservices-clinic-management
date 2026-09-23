189fbd0f7d8f16a8132eb3fb4d136bfe33607e2b
sandeepreddam <sandeepreddam11@gmail.com>
2026-09-23 21:37:54 +0000

    Update GitHub OIDC trust policy

--- devops/terraform/github-actions-iam.tf
+++ devops/terraform/github-actions-iam.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "github_actions" {
           }
 
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:sandeepreddam/spring-microservices-clinic-management:*"
+            "token.actions.githubusercontent.com:sub" = "repo:sandeepreddam@196306213/spring-microservices-clinic-management@1384195969:ref:refs/heads/main"
           }
         }
       }
